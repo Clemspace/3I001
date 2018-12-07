e93f72a60627a80ceeb6d5e98bf9bb1e102f43b5
Clemspace <clement.castellon@gmail.com>
2018-12-07 12:21:24 +0100

    ex 14 updated

--- Exercice14/Corde.java
+++ Exercice14/Corde.java
@@ -1,13 +1,19 @@
 import java.util.Concurrent.*;
 
 public class Corde(){
-  //gérer la section critique, soit locks soit semaphores
+  //gérer la section critique, soit locks, soit semaphores
 
 
   public void acceder(Position pos){
 
   }
   public void lacher(Position pos){
+
+  }
+  public void accederKong(Position pos){
+
+  }
+  public void lacherKong(Position pos){
 
   }
 }
--- Exercice14/TestBaboons.java
+++ Exercice14/TestBaboons.java
@@ -2,12 +2,21 @@
 public static void main(Str[] args){
 
   final int NB_BABOUINS = 15;
+  final int NB_KONGS = 1;
   Corde c = new Corde();
+
+  //TODO gérer accès aux threads pour le join des classes
 
   for(int i = 0; i< NB_BABOUINS; i++){
     Thread t = newThread(new Babouin(c)).start();
   }
+  for(int i = 0; i< NB_KONGS; i++){
+    Thread t = newThread(new Kong(c)).start();
+  }
   for(int i = 0; i< NB_BABOUINS; i++){
+    t.join();
+  }
+  for(int i = 0; i< NB_KONGS; i++){
     t.join();
   }
   System.out.println("Fin de l'éxécution, bisous!");
